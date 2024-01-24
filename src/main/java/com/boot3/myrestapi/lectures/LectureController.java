9d3ffc0685ca9f036ac6166bdff4cafab4f1ca90
mysoyul <vega2k_@naver.com>
2024-01-24 14:36:25 +0900

    LectureController update

--- src/main/java/com/boot3/myrestapi/lectures/LectureController.java
+++ src/main/java/com/boot3/myrestapi/lectures/LectureController.java
@@ -1,5 +1,6 @@
 package com.boot3.myrestapi.lectures;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -12,14 +13,24 @@ import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/api/lectures", produces = MediaTypes.HAL_JSON_VALUE)
+@RequiredArgsConstructor
 public class LectureController {
+
+    private final LectureRepository lectureRepository;
+
+    //constructor injection
+//    public LectureController(LectureRepository lectureRepository) {
+//        this.lectureRepository = lectureRepository;
+//    }
+
     @PostMapping
     public ResponseEntity<?> createLecture(@RequestBody Lecture lecture) {
-        lecture.setId(10);
+        Lecture addedLecture = lectureRepository.save(lecture);
+
         // api/lectures/10 링크를 생성
         WebMvcLinkBuilder selfLinkBuilder =
-                WebMvcLinkBuilder.linkTo(LectureController.class).slash(lecture.getId());
+                WebMvcLinkBuilder.linkTo(LectureController.class).slash(addedLecture.getId());
         URI createUri = selfLinkBuilder.toUri();
-        return ResponseEntity.created(createUri).body(lecture);
+        return ResponseEntity.created(createUri).body(addedLecture);
     }
 }
