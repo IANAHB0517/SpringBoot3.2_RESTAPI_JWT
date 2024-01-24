c006af18a1689448451bfd991c1ca77a49aa0ee9
mysoyul <vega2k_@naver.com>
2024-01-24 11:34:17 +0900

    LectureController add

--- src/test/java/com/boot3/myrestapi/lectures/LectureTest.java
+++ src/test/java/com/boot3/myrestapi/lectures/LectureTest.java
@@ -8,20 +8,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class LectureTest {
     @Test
     public void builder() {
-        Lecture lecture = Lecture.builder()
+        Lecture lecture = Lecture.builder() //LectureBuilder
                 .name("Spring REST API")
-                .description("REST API developmemt with Spring")
-                .build();
+                .description("REST API development with Spring")
+                .basePrice(100)
+                .build();  //Lecture
         assertEquals("Spring REST API", lecture.getName());
+        assertEquals(100, lecture.getBasePrice());
     }
+
     @Test
     public void javaBean()
     {
         String name = "Lecture"; //Give
         String description = "Spring";
+
         Lecture lecture = new Lecture(); //When
         lecture.setName(name);
         lecture.setDescription(description);
+
         assertThat(lecture.getName()).isEqualTo("Lecture"); //Then
         assertThat(lecture.getDescription()).isEqualTo("Spring");
     }
