ad3b56c663f36463703092856f4aa68992413989
mysoyul <vega2k_@naver.com>
2024-01-24 17:09:27 +0900

    ErrorsSerializer ãupdate

--- src/main/java/com/boot3/myrestapi/common/errors/ErrorsSerializer.java
+++ src/main/java/com/boot3/myrestapi/common/errors/ErrorsSerializer.java
@@ -14,14 +14,15 @@ public class ErrorsSerializer extends JsonSerializer<Errors>{
 	@Override
 	public void serialize(Errors errors, JsonGenerator gen, SerializerProvider serializers) throws IOException {
 		gen.writeStartArray();
+        //FieldError 정보 직렬화
         errors.getFieldErrors().forEach(e -> {
             try {
                 gen.writeStartObject();
-                gen.writeStringField("field", e.getField());
-                gen.writeStringField("objectName", e.getObjectName());
-                gen.writeStringField("code", e.getCode());
-                gen.writeStringField("defaultMessage", e.getDefaultMessage());
-                Object rejectedValue = e.getRejectedValue();
+                gen.writeStringField("field", e.getField()); //"name"
+                gen.writeStringField("objectName", e.getObjectName()); //ReqDto
+                gen.writeStringField("code", e.getCode()); //NotBlank
+                gen.writeStringField("defaultMessage", e.getDefaultMessage()); //Name은 필수 입력 항목입니다.
+                Object rejectedValue = e.getRejectedValue();  //잘못 입력된 값
                 if (rejectedValue != null) {
                     gen.writeStringField("rejectedValue", rejectedValue.toString());
                 }
@@ -31,6 +32,7 @@ public class ErrorsSerializer extends JsonSerializer<Errors>{
             }
         });
 
+        //GlobalError 정보 직렬화
         errors.getGlobalErrors().forEach(e -> {
             try {
                 gen.writeStartObject();
