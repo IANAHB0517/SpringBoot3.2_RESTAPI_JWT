b83114d95a58dc20618aa40e4f3036562c16d211
mysoyul <vega2k_@naver.com>
2024-01-24 11:11:12 +0900

    initial commit

---
+++ src/main/java/com/boot3/myrestapi/Boot3RestJwtApplication.java
@@ -0,0 +1,14 @@
+package com.boot3.myrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Boot3RestJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot3RestJwtApplication.class, args);
+	}
+
+}
